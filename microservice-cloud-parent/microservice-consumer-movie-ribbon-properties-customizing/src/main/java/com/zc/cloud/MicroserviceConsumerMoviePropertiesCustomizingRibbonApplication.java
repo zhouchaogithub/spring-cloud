d8e5a90b1594230642b338954a8404153ede5ea9
zhouchao <zhouchaoit@sina.com>
2018-09-29 22:42:38 +0800

    配置文件设置ribbon负载均衡和将.properties文件转换成.yml文件

---
+++ microservice-cloud-parent/microservice-consumer-movie-ribbon-properties-customizing/src/main/java/com/zc/cloud/MicroserviceConsumerMoviePropertiesCustomizingRibbonApplication.java
@@ -0,0 +1,22 @@
+package com.zc.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MicroserviceConsumerMoviePropertiesCustomizingRibbonApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMoviePropertiesCustomizingRibbonApplication.class, args);
+    }
+    @Bean
+    @LoadBalanced //ribbon负载均衡（默认是轮询的方式实现负载均衡）
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}

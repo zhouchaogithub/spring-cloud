3b2b1b912ca206e99f39a60ad7d4761651efe06a
zhouchao <zhouchaoit@sina.com>
2018-09-30 17:56:57 +0800

    Feign logging配置

--- microservice-cloud-parent/microservice-consumer-movie-feign-customizing/src/main/java/com/zc/config/FooConfiguration.java
+++ microservice-cloud-parent/microservice-consumer-movie-feign-customizing/src/main/java/com/zc/config/FooConfiguration.java
@@ -1,6 +1,7 @@
 package com.zc.config;
 
 import feign.Contract;
+import feign.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +14,12 @@ import org.springframework.context.annotation.Configuration;
 public class FooConfiguration {
     @Bean
     public Contract feignContract() {
+
         return new feign.Contract.Default();
     }
+    @Bean
+    Logger.Level feignLoggerLevel() {
 
+        return Logger.Level.FULL;
+    }
 }
